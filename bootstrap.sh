71e49aea25c604d2fedbc1cb1cfcb5358e7875b7
tuxpeople <thomas@tuxpeople.org>
2020-02-20 14:37:07 +0100

    versions in vars

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@ AZ_LOCATION=westeurope
 R_NODENAME=rancher
 R_NODEUSER=rancher
 R_NODESSH="~/.ssh/id_rsa.pub"
+RKE_VERSION=1.0.4
+HELM_VERSION=3.1.0
+CERTMANAGER_VERSION=0.13
 
 # Vars which get auto assembled
 R_NODEDNS=${R_NODENAME}-$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-z0-9' | fold -w 6 | head -n 1)
@@ -32,6 +35,9 @@ echo "R_REMOTEIP=$(az vm list-ip-addresses -n ${R_NODENAME} | grep ipAddress | c
 echo "R_LOCALIP=$(az vm list-ip-addresses -n ${R_NODENAME} | grep -A 1 privateIpAddresses | tail -1 | cut -d'"' -f2)" >> params.txt
 echo "R_NODEUSER=${R_NODEUSER}" >> params.txt
 echo "R_NODEFQDN=${R_NODEFQDN}" >> params.txt
+echo "RKE_VERSION=${RKE_VERSION}" >> params.txt
+echo "HELM_VERSION=${HELM_VERSION}" >> params.txt
+echo "CERTMANAGER_VERSION=${CERTMANAGER_VERSION}" >> params.txt
 
 # Upload stage 2 and params to new vm and execute it
 scp install.sh ${R_NODEUSER}@${R_NODEFQDN}:
